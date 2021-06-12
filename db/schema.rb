c9bd3c45bd1963fd52af4ad1df7b887eff094f99
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-06-12 13:25:11 +0900

    ログインの有無によりヘッダーの表示を変更した

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_062028) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
+    t.string "family_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
