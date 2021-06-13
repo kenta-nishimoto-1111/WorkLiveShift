9ec96e1a0b433ce6951b7e302782d9f5124e3c05
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-06-13 12:34:20 +0900

    性格診断のビュー画面を記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations',
                                    sessions: 'users/sessions' }
-                                   
   root 'companies#index'
-  resources :personalities ,only:[:index]
-end
+
+  resources :personalities ,only:[:index] do
+    collection do 
+      get 'first_question'
+     end
+     collection do 
+      get 'second_question'
+     end
+    end
+  end
 
