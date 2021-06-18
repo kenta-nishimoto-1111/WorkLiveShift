8abe2ac522793db6e685b611b6f3814f77d11889
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-06-18 10:42:45 +0900

    基本情報追加のビュー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_010812) do
+ActiveRecord::Schema.define(version: 2021_06_18_011434) do
+
+  create_table "company_names", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "position", null: false
+    t.integer "period", null: false
+    t.text "business_content", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_company_names_on_user_id"
+  end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "age", null: false
@@ -67,5 +78,6 @@ ActiveRecord::Schema.define(version: 2021_06_18_010812) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "company_names", "users"
   add_foreign_key "profiles", "users"
 end
