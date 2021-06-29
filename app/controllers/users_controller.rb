439f95fe40acd73a5e73b50b9846796ff7100685
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-06-29 15:40:41 +0900

    ユーザー詳細にも分析結果を表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @profile = @user.profile
     @career = @user.career
+    answers = current_user.user_question_answers
+    @total = {"hamo"=>0, "pasi"=>0, "revel"=>0, "thinker"=>0, "ima"=>0, "pro"=>0}
+    answers.each do |answer|
+      @total[answer.personality_type] += answer.point  
+    end
 
-    @birthday = @profile.birth_date
-    date_format = "%Y%m%d"
-    birthday = @birthday.to_date
-    @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    if @profile.present?
+        @birthday = @profile.birth_date
+        date_format = "%Y%m%d"
+        birthday = @birthday.to_date
+        @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    end
   end
 end
