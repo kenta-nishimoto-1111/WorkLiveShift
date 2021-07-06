54219e6ddbc7522ef0855507c7d647cab822f6ce
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-06 18:53:07 +0900

    保存機能の実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,6 +5,8 @@ class ChatsController < ApplicationController
   def show
     @chat_room = Chat.find(params[:id])
     @messages = @chat_room.messages
+
+    @message = Message.new
   end
 
   def new
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,3 @@
 class Message < ApplicationRecord
-  belongs_to :message
+  belongs_to :chat
 end
