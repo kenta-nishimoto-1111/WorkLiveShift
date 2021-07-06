b292fb78a1d6981afe9db76180987cf355ed49c6
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-06 09:02:52 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_054902) do
+ActiveRecord::Schema.define(version: 2021_07_05_235709) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_07_02_054902) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["position"], name: "index_careers_on_position"
     t.index ["user_id"], name: "index_careers_on_user_id"
+  end
+
+  create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "company_id"
+    t.text "message", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_chats_on_company_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -135,6 +145,8 @@ ActiveRecord::Schema.define(version: 2021_07_02_054902) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "careers", "users"
+  add_foreign_key "chats", "companies"
+  add_foreign_key "chats", "users"
   add_foreign_key "favorites", "companies"
   add_foreign_key "favorites", "users"
 end
