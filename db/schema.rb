27d089680dd8bb213078196a6ca80a4b02ca46c9
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-07 16:35:55 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_063546) do
+ActiveRecord::Schema.define(version: 2021_07_06_084753) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -78,20 +78,6 @@ ActiveRecord::Schema.define(version: 2021_07_07_063546) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_favorites_on_company_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
-  end
-
-  create_table "job_offers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "company_name", default: "", null: false
-    t.string "company_image", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_job_offers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_job_offers_on_reset_password_token", unique: true
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
