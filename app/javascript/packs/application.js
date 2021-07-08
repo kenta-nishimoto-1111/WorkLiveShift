1d492a67c79e23292c1df05f1203c4f6511e5ec0
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-08 13:50:05 +0900

    不要なファイル削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('../form_height')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
