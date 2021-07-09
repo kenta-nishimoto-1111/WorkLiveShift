2d9b5e62e8240d79fa3836db8828aca67e2d29e4
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-09 09:00:25 +0900

    送信者によってチャット画面の表示を変更させる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 class MessagesController < ApplicationController
   def create
+
+    if current_user
+      @sent_person = current_user.id
+    elsif current_supplier
+      @sent_person = current_supplier.id
+    end
+
     @chat_room = Chat.find(params[:chat_id])
     @message = @chat_room.messages.new(message_params)
     if @message.save
@@ -12,6 +19,6 @@ class MessagesController < ApplicationController
   private
   
     def message_params
-      params.require(:message).permit(:content).merge(sent_person: current_user.id)
+      params.require(:message).permit(:content).merge(sent_person: @sent_person)
     end
 end
