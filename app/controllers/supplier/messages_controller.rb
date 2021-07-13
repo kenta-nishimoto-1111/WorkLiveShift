56062d1ba825179f54180cdb73daabc2efcb2523
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-13 10:09:57 +0900

    パスの変更

---
+++ app/controllers/supplier/messages_controller.rb
@@ -0,0 +1,19 @@
+class Supplier::MessagesController < ApplicationController
+  def create
+    if current_user
+      @sent_person = current_user.family_name
+    elsif current_supplier
+      @sent_person = current_supplier.company_name
+    end
+    @chat_room = Chat.find(params[:chat_id])
+    @message = @chat_room.messages.new(message_params)
+    @chat_room.update(latest_message: @message.content, latest_message_time: Time.current) if @message.save
+    redirect_to supplier_chat_path(@chat_room)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(sent_person: @sent_person)
+  end
+end
