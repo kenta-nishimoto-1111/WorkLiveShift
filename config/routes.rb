e9dbc75cca656830ad253445c3068b7bfd0d5496
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-21 09:05:05 +0900

    企業情報編集のパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
       resources :messages, only: %i[new create]
     end
 
-    resources :companies, only: %i[index create show] do
+    resources :companies, only: %i[index create show edit] do
       collection do
         get 'first_step'
         get 'second_step'
