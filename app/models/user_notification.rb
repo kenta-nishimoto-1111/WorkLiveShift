b4c11ae1e0a6ffec477e6d0121a530623929c70a
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-23 11:38:57 +0900

    Revert "ユーザーの通知テーブル作成"
    This reverts commit 1cf9e877d8ea47957b4f82553ee23954109b57fb.

--- app/models/user_notification.rb
+++
@@ -1,2 +0,0 @@
-class UserNotification < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_022522) do
+ActiveRecord::Schema.define(version: 2021_07_13_024021) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -172,15 +172,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_022522) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_suppliers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_suppliers_on_reset_password_token", unique: true
-  end
-
-  create_table "user_notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "visitor_id", null: false
-    t.integer "visited_id", null: false
-    t.integer "chat_id"
-    t.boolean "checked", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "user_question_answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
