874c186bb998992e29ccdf9c6bfe8d70f26fe341
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-27 12:16:29 +0900

    企業画像用のテーブル作成

---
+++ app/models/image_other_company.rb
@@ -0,0 +1,2 @@
+class ImageOtherCompany < ApplicationRecord
+end
---
+++ test/models/image_other_company_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ImageOtherCompanyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
