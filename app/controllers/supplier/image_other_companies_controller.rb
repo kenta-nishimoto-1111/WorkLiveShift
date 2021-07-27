aaca02d6baf60050304387d2b1d493178299b479
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-07-27 13:16:54 +0900

    画像保存機能実装

--- app/controllers/supplier/image_other_companies_controller.rb
+++ app/controllers/supplier/image_other_companies_controller.rb
@@ -1,6 +1,20 @@
 class Supplier::ImageOtherCompaniesController < ApplicationController
 
   def new
-    @image_other_company = ImageOtherCompany.new
+    @image = ImageOtherCompany.new
+  end
+
+  def create
+    @image = ImageOtherCompany.new(image_other_company_params)
+    if @image.save
+      redirect_to company_details_supplier_company_path(current_supplier)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def image_other_company_params
+    params.require(:image_other_company).permit(:image_other_company).merge(supplier_id: current_supplier.id)
   end
 end
--- app/models/image_other_company.rb
+++ app/models/image_other_company.rb
@@ -1,4 +1,6 @@
 class ImageOtherCompany < ApplicationRecord
   belongs_to :supplier
   has_one_attached :image_other_company
+
+  validates :image_other_company, presence: true
 end
