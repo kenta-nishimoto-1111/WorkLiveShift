d4fe2acec5701928643a19b9851ede79fcbd8170
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-08-03 08:50:26 +0900

    企業診断の保存機能実装

--- app/models/supplier_question_answer.rb
+++ app/models/supplier_question_answer.rb
@@ -3,7 +3,6 @@ class SupplierQuestionAnswer < ApplicationRecord
 
   belongs_to :supplier
   belongs_to :supplier_question
-  belongs_to :supplier_question_option
 
   with_options presence: true, numericality: { other_than: 0 } do
     validates :point
--- db/migrate/20210801060134_create_supplier_question_answers.rb
+++ db/migrate/20210801060134_create_supplier_question_answers.rb
@@ -2,7 +2,7 @@ class CreateSupplierQuestionAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :supplier_question_answers do |t|
       t.integer :supplier_id, null: false
-      t.integer :question_id,   null: false
+      t.integer :supplier_question_id,   null: false
       t.string  :personality_type, null: false
       t.integer :point, null: false
       t.timestamps
