f45b3346722b2197563c550acd0eed455d763ddd
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-08-08 11:01:16 +0900

    企業一覧のスライドショー作成

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -6,3 +6,13 @@ $(function() {
       slidesToShow: 3,
   });
 });
+
+$(function() {
+  $('.slider_top').slick({
+      arrows: false,  //ボタン非表示
+      autoplay: true, //自動再生
+      autoplaySpeed: 2000, //再生スピード
+      centerMode: true,
+      centerPadding: '14%',
+  });
+});
