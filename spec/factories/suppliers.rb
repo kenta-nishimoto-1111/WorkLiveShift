0147df84ebd0042a293b46beefdd705a01dc8ea0
kenta-nishimoto-1111 <0tb1161j@gmail.com>
2021-08-20 12:17:53 +0900

    企業情報の保存の際のテストコード

--- spec/factories/suppliers.rb
+++ spec/factories/suppliers.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :supplier do
-    company_name { 'test' }
-    sequence(:email) { |n| "test_user_email_#{n}@example.com" }
-    password { 'test1111' }
+    company_name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
     after(:build) do |supplier|
-      supplier.company_image.attach(io: File.open(Rails.root.join('app/assets/images', 'test.jpg')), filename: 'test.jpg')
+      supplier.company_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,6 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe Supplier, type: :model do
-  describe 'validation' do
+RSpec.describe User, type: :model do
+  before do
+    @supplier = FactoryBot.build(:supplier)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'company_nameとcompany_image、email、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@supplier).to be_valid
+    end
+    
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      @supplier.password = '000000'
+      @supplier.password_confirmation = '000000'
+      expect(@supplier).to be_valid
+    end
+    
+    it 'nicknameが空では登録できない' do
+      @supplier.company_name = ''
+      @supplier.valid?
+      expect(@supplier.errors.full_messages).to include("Company name can't be blank")
+    end
+
+    it 'emailが空では登録できない' do
+      @supplier.email = ''
+      @supplier.valid?
+      expect(@supplier.errors.full_messages).to include("Email can't be blank")
+    end
+    
+    it 'passwordが空では登録できない' do
+      @supplier.password = ''
+      @supplier.valid?
+      expect(@supplier.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      @supplier.password_confirmation = ''
+      @supplier.valid?
+      expect(@supplier.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it '重複したemailが存在する場合登録できない' do
+      @supplier.save
+      another_user = FactoryBot.build(:supplier)
+      another_user.email = @supplier.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it 'passwordが5文字以下では登録できない' do
+      @supplier.password = '00000'
+      @supplier.password_confirmation = '00000'
+      @supplier.valid?
+      expect(@supplier.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
   end
 end
