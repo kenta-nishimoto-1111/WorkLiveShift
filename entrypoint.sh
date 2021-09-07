32f5b331205858a64aaf588c80e7ad48a26f6963
Yusuke Yasuo <yasuo.y@dely.jp>
2021-09-08 00:50:14 +0900

    deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
+yarn install --pure-lockfile
+bin/webpack-dev-server &
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
